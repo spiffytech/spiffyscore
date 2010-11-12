2e202e38ac3ed9f39b3b8abc242505d3ea47403e
spiffytech@gmail.com <spiffytech@gmail.com>
2010-11-12 20:12:48 +0000

    Moved all logic into neat, orderly functions. Now prints only the csound score portion.

--- cfg.py
+++ cfg.py
@@ -11,65 +11,80 @@ grammars = {
     "e": [""],
 }
 
-# Generate the scale for the key we're in
-comp_key = "C"
-notes = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"]
-scale = [comp_key]
-pos = notes.index(comp_key)
-progression = [2,2,1,2,2,2,1]
-for p in progression:
-    pos = (pos + p) % 12
-    scale.append(notes[pos])
 
-scale_conversion = {
-    "I": 1,
-    "II": 2,
-    "III": 3,
-    "IV": 4,
-    "V": 5,
-    "VI": 6,
-    "VII": 7,
-    "VIII": 8,
-}
-
-csound_note_values = {
-    "C": "00",
-    "C#": "01",
-    "D": "02",
-    "D#": "03",
-    "E": "04",
-    "F": "05",
-    "F#": "06",
-    "G": "07",
-    "F#": "08",
-    "A": "09",
-    "A#": "10",
-    "B": "11",
-}
+def main():
+    score = "u u u"
+    key = "C"
+    score = generate_score(score)
+    score = keyify_score(score, key)
+    score = generate_csound_score(score)
+    for line in score:
+        print line
 
 
-score = "u u u"
-print score
-while 1:
-    found_substitution = False
-    for key,value in grammars.iteritems():
-        if score.find(key) != -1:
-            found_substitution = True
-            while score.find(key) != -1:
-                score = score.replace(key, random.choice(grammars[key]), 1)
-                print score
-        #        time.sleep(.25)
-    if found_substitution is False:
-        break
+def make_scale(key):
+    notes = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"]
+    scale = [key]
+    pos = notes.index(key)
+    progression = [2,2,1,2,2,2,1]
+    for p in progression:
+        pos = (pos + p) % 12
+        scale.append(notes[pos])
+    return scale
 
-csound_score = []
-for token in score.split():
-    csound_score.append(scale[scale_conversion[token]-1])
 
-print csound_score
+def generate_score(score):
+    while 1:
+        found_substitution = False
+        for key,value in grammars.iteritems():
+            if score.find(key) != -1:
+                found_substitution = True
+                while score.find(key) != -1:
+                    score = score.replace(key, random.choice(grammars[key]), 1)
+        if found_substitution is False:
+            break
+    return score
 
-t = 0 
-for token in csound_score:
-    note = csound_note_values[token]
-    print "i2 %f 2 7000 %d.%s %d.%s 0 6" % (t, random.choice([8,9]), note, random.choice([8,9]), note)
-    t += .25
+def keyify_score(score, key):
+    scale = make_scale(key)
+    scale_conversion = {
+        "I": 1,
+        "II": 2,
+        "III": 3,
+        "IV": 4,
+        "V": 5,
+        "VI": 6,
+        "VII": 7,
+        "VIII": 8,
+    }
+    keyed_score = []
+    for token in score.split():
+        keyed_score.append(scale[scale_conversion[token]-1])
+    return keyed_score
+
+
+def generate_csound_score(score):
+    csound_note_values = {
+        "C": "00",
+        "C#": "01",
+        "D": "02",
+        "D#": "03",
+        "E": "04",
+        "F": "05",
+        "F#": "06",
+        "G": "07",
+        "F#": "08",
+        "A": "09",
+        "A#": "10",
+        "B": "11",
+    }
+    t = 0 
+    csound_score = []
+    for token in score:
+        note = csound_note_values[token]
+        csound_score.append("i2 %f 2 7000 %d.%s %d.%s 0 6" % (t, random.choice([8,9]), note, random.choice([8,9]), note))
+        t += .25
+    return csound_score
+
+
+if __name__ == "__main__": main() 
