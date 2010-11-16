73ef0f2f13057ccd08ca5704228042d36a6a268c
spiffytech@gmail.com <spiffytech@gmail.com>
2010-11-16 18:37:14 +0000

    Now prints csound code, both notes and chords, replicating the functionality of the original Minimaly Functional Version

--- cfg.py
+++ cfg.py
@@ -5,6 +5,7 @@ import random
 import sys
 import time
 random.seed(time.time())
+import parse
 
 def main():
     key = "A"
@@ -13,7 +14,7 @@ def main():
         "e": [""],
     }
     chord_grammars = {
-        "u": ["I IV V IV I u u", "I VII IV u u", "I V IV u u", "e"],
+        "u": ['"I" "IV" "V" "IV" "I" u u', '"I" "VII" "IV" u u', '"I" "V" "IV" u u', "e"],
         "e": [""]
     }
     compose_piece(key, note_grammars)
@@ -21,9 +22,10 @@ def main():
 
 def compose_piece(key, grammars, chords=False):
     score = ""
-    while len(score.split()) < 200:
+    while len(score.split()) < 10:
         score = "u u u"
         score = generate_score(score, grammars)
+    score = parse.parse(score)
     score = transliterate_score(score, key, chords)
     score = generate_csound_score(score)
     print "f1  0   256 10  1 0 3   ; sine wave function table"
@@ -50,6 +52,10 @@ def generate_score(score, grammars):
                 found_substitution = True
                 while score.find(key) != -1:
                     score = score.replace(key, random.choice(grammars[key]), 1)
+                    if len(score) > 200:
+                        score = score.replace("u", "")
+                        score = score.replace("e", "")
+                        return score
         if found_substitution is False:
             break
     return score
@@ -68,17 +74,17 @@ def transliterate_score(score, key, chords=False):
     }
     keyed_score = []
     if chords is False:
-        for token in score.split():
-            keyed_score.append(scale[scale_conversion[token]-1])
+        for i in range(len(score)):
+            score[i].value = scale[scale_conversion[score[i].value]-1]
     else:
-        for token in score.split():
+        for i in range(len(score)):
             chord = []
-            root_note_index = scale.index(key) + scale_conversion[token]
+            root_note_index = scale.index(key) + scale_conversion[score[i].value]
             chord.append(scale[root_note_index])
             chord.append(scale[(root_note_index+3) % 8])
             chord.append(scale[(root_note_index+5) % 8])
-            keyed_score.append(chord)
-    return keyed_score
+            score[i].chord = chord
+    return score
 
 
 def generate_csound_score(score):
@@ -99,13 +105,13 @@ def generate_csound_score(score):
     t = 0 
     csound_score = []
     for token in score:
-        if isinstance(token, list):  # Chords
-            for note in token: 
+        if isinstance(token, parse.Chord):  # Chords
+            for note in token.chord: 
                 note = csound_note_values[note]
                 csound_score.append("i2 %(time)f 1 7000 %(octave)d.%(note)s %(octave)d.%(note)s 0 6" % {"time": t, "octave": random.choice([7,8]), "note": note})
             t += 1
         else:  # Individual notes
-            note = csound_note_values[token]
+            note = csound_note_values[token.value]
             csound_score.append("i2 %(time)f 1 7000 %(octave)d.%(note)s %(octave)d.%(note)s 0 6" % {"time": t, "octave": random.choice([8,9]), "note": note})
             t += .25
     return csound_score
--- parse.py
+++ parse.py
@@ -1,52 +1,6 @@
 #!/usr/bin/env python
 
 from ply import lex, yacc
-
-# Tokenize (lex)
-tokens = (
-    "NOTE_LENGTH",
-    "BASENOTE",
-    "ACCIDENTAL",
-    "REST",
-    "OCTAVE",
-    "CHORD_TYPE",
-    "QUOTE",
-)
-
-t_ignore = " |"
-
-#t_BASENOTE = r"[A-Ga-g]"
-t_BASENOTE = r"I+V?|VI*|i+v?|vi*"
-t_ACCIDENTAL = r"\^{1,2}|_{1,2}|="
-t_REST = r"z"
-t_OCTAVE = r"'+|,+"
-t_CHORD_TYPE = r"m|7|m7|0|o|\+|mb5|sus|sus4|maj7|mmaj7|7sus4|dim|dim7|7b5|m7b5|6|b6|m6|mb6|46|maj9|9|add9|7b9|m9"
-t_QUOTE = '"'
-
-def t_NOTE_LENGTH(t):
-    r"/?\d+"
-    multiplier = float(t.value.strip("/"))
-    if t.value.startswith("/"):
-        multiplier = 1/multiplier
-    t.value = multiplier
-    return t
-
-def t_error(t):
-    raise TypeError("Unknown text '%s'" % (t.value,))
-
-lex.lex()
-
-#lex.input("GFG B'AB,, | g/2fg gab | GFG BAB | d2A AFD")
-#s = "GFG B'AB,, | g/2fg gab | GFG BAB | d2A AFD"
-s = '''I IV V VI I "I" "ii"/2'''
-#s = "GF_G,/2"
-lex.input(s)
-for tok in iter(lex.token, None):
-    print repr(tok.type), repr(tok.value)
-
-
-# Parse (yacc)
-
 class Note():
     def __init__(self, value, duration=.25, octave=8):
         self.value = value
@@ -65,73 +19,120 @@ class Chord():
         return "Chord %s %s %s" % (self.value, self.duration, self.chord_type)
 
 
-def p_pitch_list(p):
-    '''score : score note
-        score : score chord
-    '''
-    p[0] = p[1] + [p[2]]
+def parse(score):
+    # Tokenize (lex)
+    tokens = (
+        "NOTE_LENGTH",
+        "BASENOTE",
+        "ACCIDENTAL",
+        "REST",
+        "OCTAVE",
+        "CHORD_TYPE",
+        "QUOTE",
+    )
 
-def p_score(p):
-    '''score : note
-        score : chord
-    '''
-    p[0] = [p[1]]
+    t_ignore = " |"
+
+    #t_BASENOTE = r"[A-Ga-g]"
+    t_BASENOTE = r"I+V?|VI*|i+v?|vi*"
+    t_ACCIDENTAL = r"\^{1,2}|_{1,2}|="
+    t_REST = r"z"
+    t_OCTAVE = r"'+|,+"
+    t_CHORD_TYPE = r"m|7|m7|0|o|\+|mb5|sus|sus4|maj7|mmaj7|7sus4|dim|dim7|7b5|m7b5|6|b6|m6|mb6|46|maj9|9|add9|7b9|m9"
+    t_QUOTE = '"'
+
+    def t_NOTE_LENGTH(t):
+        r"/?\d+"
+        multiplier = float(t.value.strip("/"))
+        if t.value.startswith("/"):
+            multiplier = 1/multiplier
+        t.value = multiplier
+        return t
+
+    def t_error(t):
+        raise TypeError("Unknown text '%s'" % (t.value,))
+
+    lex.lex()
+
+    #lex.input("GFG B'AB,, | g/2fg gab | GFG BAB | d2A AFD")
+    #s = "GFG B'AB,, | g/2fg gab | GFG BAB | d2A AFD"
+    #s = '''I IV V VI I "I" "ii"/2'''
+    #s = "GF_G,/2"
+    lex.input(score)
+    #for tok in iter(lex.token, None):
+    #    print repr(tok.type), repr(tok.value)
 
 
-def p_note(p):
-    '''note : pitch
-    '''
-    p[0] = p[1]
+    # Parse (yacc)
 
 
-def p_note_length(p):
-    ''' note : note NOTE_LENGTH
-    '''
-    new_note = p[1]
-    new_note.duration = p[2]
-    p[0] = new_note
+    def p_pitch_list(p):
+        '''score : score note
+            score : score chord
+        '''
+        p[0] = p[1] + [p[2]]
 
-def p_chord_length(p):
-    ''' chord : chord NOTE_LENGTH
-    '''
-    new_note = p[1]
-    new_note.duration = p[2]
-    p[0] = new_note
+    def p_score(p):
+        '''score : note
+            score : chord
+        '''
+        p[0] = [p[1]]
 
 
-def p_chord(p):
-    '''chord : QUOTE pitch QUOTE
-            | QUOTE pitch CHORD_TYPE QUOTE
-    '''
-    p[0] = Chord(value=p[2].value)
-    if len(p) > 3:
-        p[0].chord_type = p[3]
+    def p_note(p):
+        '''note : pitch
+        '''
+        p[0] = p[1]
 
 
-def p_accidental(p):
-    '''pitch : ACCIDENTAL pitch
-    '''
-    p[2].accidental = p[1]
-    p[0] = p[2]
+    def p_note_length(p):
+        ''' note : note NOTE_LENGTH
+        '''
+        new_note = p[1]
+        new_note.duration = p[2]
+        p[0] = new_note
 
-def p_octave(p):
-    '''pitch : pitch OCTAVE
-    '''
-    count = len(p[2])
-    increment_or_decrement = 1 if p[2][0] == "," else -1
-    octave = 8 + (count * increment_or_decrement)
-    p[1].octave = octave
-    p[0] = p[1]
+    def p_chord_length(p):
+        ''' chord : chord NOTE_LENGTH
+        '''
+        new_note = p[1]
+        new_note.duration = p[2]
+        p[0] = new_note
 
-def p_pitch(p):
-    '''pitch : BASENOTE
-    '''
-    p[0] = Note(p[1])
 
-def p_error(p):
-    print "Syntax error at '%s' of element type %s" % (p.value, p.type)
-    
-yacc.yacc()
+    def p_chord(p):
+        '''chord : QUOTE pitch QUOTE
+                | QUOTE pitch CHORD_TYPE QUOTE
+        '''
+        p[0] = Chord(value=p[2].value)
+        if len(p) > 3:
+            p[0].chord_type = p[3]
 
-#print yacc.parse("GFG B'AB,, | g/2fg gab | GFG BAB | d2A AFD")
-print yacc.parse(s)
+
+    def p_accidental(p):
+        '''pitch : ACCIDENTAL pitch
+        '''
+        p[2].accidental = p[1]
+        p[0] = p[2]
+
+    def p_octave(p):
+        '''pitch : pitch OCTAVE
+        '''
+        count = len(p[2])
+        increment_or_decrement = 1 if p[2][0] == "," else -1
+        octave = 8 + (count * increment_or_decrement)
+        p[1].octave = octave
+        p[0] = p[1]
+
+    def p_pitch(p):
+        '''pitch : BASENOTE
+        '''
+        p[0] = Note(p[1])
+
+    def p_error(p):
+        print "Syntax error at '%s' of element type %s" % (p.value, p.type)
+        
+    yacc.yacc()
+
+    #print yacc.parse("GFG B'AB,, | g/2fg gab | GFG BAB | d2A AFD")
+    return yacc.parse(score)
