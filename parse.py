a25616fdccaae5bb2358ea16f98a806d2ce84852
spiffytech@gmail.com <spiffytech@gmail.com>
2010-11-16 16:34:11 +0000

    Now parses chords

--- parse.py
+++ parse.py
@@ -10,6 +10,7 @@ tokens = (
     "REST",
     "OCTAVE",
     "CHORD_TYPE",
+    "QUOTE",
 )
 
 t_ignore = " |"
@@ -20,6 +21,7 @@ t_ACCIDENTAL = r"\^{1,2}|_{1,2}|="
 t_REST = r"z"
 t_OCTAVE = r"'+|,+"
 t_CHORD_TYPE = r"m|7|m7|0|o|\+|mb5|sus|sus4|maj7|mmaj7|7sus4|dim|dim7|7b5|m7b5|6|b6|m6|mb6|46|maj9|9|add9|7b9|m9"
+t_QUOTE = '"'
 
 def t_NOTE_LENGTH(t):
     r"/?\d+"
@@ -36,7 +38,7 @@ lex.lex()
 
 #lex.input("GFG B'AB,, | g/2fg gab | GFG BAB | d2A AFD")
 #s = "GFG B'AB,, | g/2fg gab | GFG BAB | d2A AFD"
-s = "I IV V VI I"
+s = '''I IV V VI I "I" "ii"'''
 #s = "GF_G,/2"
 lex.input(s)
 for tok in iter(lex.token, None):
@@ -45,7 +47,7 @@ for tok in iter(lex.token, None):
 
 # Parse (yacc)
 
-class Note(object):
+class Note():
     def __init__(self, value, duration=.25, octave=8):
         self.value = value
         self.duration = duration
@@ -54,13 +56,24 @@ class Note(object):
     def __repr__(self):
         return "Note %s %s %s" % (self.value, self.duration, self.octave)
 
+class Chord():
+    def __init__(self, value, duration=.25, chord_type="major"):
+        self.value = value
+        self.duration = duration
+        self.chord_type = chord_type
+    def __repr__(self):
+        return "Chord %s %s %s" % (self.value, self.duration, self.chord_type)
+
+
 def p_pitch_list(p):
     '''score : score note
+        score : score chord
     '''
     p[0] = p[1] + [p[2]]
 
 def p_score(p):
     '''score : note
+        score : chord
     '''
     p[0] = [p[1]]
 
@@ -77,6 +90,16 @@ def p_note_length(p):
     new_note = p[1]
     new_note.duration = p[2]
     p[0] = new_note
+
+
+def p_chord(p):
+    '''chord : QUOTE pitch QUOTE
+            | QUOTE pitch CHORD_TYPE QUOTE
+    '''
+    p[0] = Chord(value=p[2].value)
+    if len(p) > 3:
+        p[0].chord_type = p[3]
+
 
 def p_accidental(p):
     '''pitch : ACCIDENTAL pitch
